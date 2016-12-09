3ff88af538db5b86b2ab60df557dd0456523fa0d
Toke Jepsen <tokejepsen@gmail.com>
2016-12-09 15:49:08 +0100

    Update maya plugins location

--- environment.py
+++ environment.py
@@ -4,6 +4,6 @@ from conda_git_deployment import utils
 
 
 path = os.path.join(
-    os.environ["CONDA_GIT_REPOSITORY"], "pyblish-tgbvfx", "plugins"
+    os.environ["CONDA_GIT_REPOSITORY"], "pyblish-tgbvfx", "plugins", "maya"
 )
 utils.write_environment({"PYBLISHPLUGINPATH": path})
