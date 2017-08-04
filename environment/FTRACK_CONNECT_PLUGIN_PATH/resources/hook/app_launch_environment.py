fac24c8ddc97fedf92f3e18c26bd1efda219094a
Toke Jepsen <tokejepsen@gmail.com>
2017-08-04 13:30:22 +0200

    Add PROJECT_PATH on application launch.

--- environment/FTRACK_CONNECT_PLUGIN_PATH/resources/hook/app_launch_environment.py
+++ environment/FTRACK_CONNECT_PLUGIN_PATH/resources/hook/app_launch_environment.py
@@ -1,6 +1,8 @@
 import os
+import platform
 
-import ftrack
+import ftrack_api
+import ftrack_connect
 
 
 def ftrack_event_plugin_path(event):
@@ -18,16 +20,40 @@ def ftrack_event_plugin_path(event):
         data["options"]["env"].get("FTRACK_EVENT_PLUGIN_PATH", "")
     )
 
+    session = ftrack_connect.session.get_shared_session()
+    task = session.get(
+        "Task", event["data"]["context"]["selection"][0]["entityId"]
+    )
+
+    system_name = platform.system().lower()
+    if system_name != "windows":
+        system_name = "unix"
+
+    # Need to enforce variable to string type, as returned type is unicode.
+    data["options"]["env"]["PROJECT_PATH"] = (
+        str(
+            os.path.abspath(
+                os.path.join(
+                    task["project"]["disk"][system_name],
+                    task["project"]["root"]
+                )
+            )
+        )
+    )
+
     return data
 
 
-def register(registry, **kw):
-    # Validate that registry is the correct ftrack.Registry. If not,
-    # assume that register is being called with another purpose or from a
-    # new or incompatible API and return without doing anything.
-    if registry is not ftrack.EVENT_HANDLERS:
+def register(session, **kw):
+    '''Register event listener.'''
+
+    # Validate that session is an instance of ftrack_api.Session. If not,
+    # assume that register is being called from an incompatible API
+    # and return without doing anything.
+    if not isinstance(session, ftrack_api.Session):
         # Exit to avoid registering this plugin again.
         return
 
+    # Register the event handler
     subscription = "topic=ftrack.connect.application.launch"
-    ftrack.EVENT_HUB.subscribe(subscription, ftrack_event_plugin_path)
+    session.event_hub.subscribe(subscription, ftrack_event_plugin_path)
