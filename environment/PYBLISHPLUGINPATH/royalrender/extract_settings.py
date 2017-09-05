fe7f4b074fafd0d26b97b78885b68ee523f68725
Toke Jepsen <tokejepsen@gmail.com>
2017-09-05 11:58:51 +0200

    Remove RoyalRender Maya settings. Target Nuke RoyalRender settings. Assign "NODES_OLD" only to nuke renders.

--- environment/PYBLISHPLUGINPATH/royalrender/extract_settings.py
+++ environment/PYBLISHPLUGINPATH/royalrender/extract_settings.py
@@ -1,29 +1,6 @@
 import pyblish.api as api
 
 
-class TGBVFXEnvironmentRoyalRenderExtractSettingsMaya(api.InstancePlugin):
-    """ Add studio submission settings. """
-
-    order = api.ExtractorOrder
-    label = "Settings"
-    families = ["royalrender"]
-    hosts = ["maya"]
-
-    def process(self, instance):
-
-        data = instance.data.get("royalrenderData", {})
-
-        # SubmitterParameter
-        submit_params = data.get("SubmitterParameter", [])
-        submit_params.append("SeqDivMin=1~1")
-        submit_params.append("SeqDivMax=1~5")
-        submit_params.append("DefaultClientGroup=1~NODES_ALL")
-        data["SubmitterParameter"] = submit_params
-
-        # Setting data
-        instance.data["royalrenderData"] = data
-
-
 class TGBVFXEnvironmentRoyalRenderExtractSettingsNuke(api.InstancePlugin):
     """ Add studio submission settings. """
 
@@ -31,6 +8,7 @@ class TGBVFXEnvironmentRoyalRenderExtractSettingsNuke(api.InstancePlugin):
     label = "Settings"
     families = ["royalrender"]
     hosts = ["nuke"]
+    targets = ["process.royalrender"]
 
     def process(self, instance):
 
@@ -40,7 +18,7 @@ class TGBVFXEnvironmentRoyalRenderExtractSettingsNuke(api.InstancePlugin):
         submit_params = data.get("SubmitterParameter", [])
         submit_params.append("SeqDivMINComp=1~5")
         submit_params.append("SeqDivMAXComp=1~40")
-        submit_params.append("DefaultClientGroup=1~NODES_NEW")
+        submit_params.append("DefaultClientGroup=1~NODES_OLD")
         data["SubmitterParameter"] = submit_params
 
         # Setting data
