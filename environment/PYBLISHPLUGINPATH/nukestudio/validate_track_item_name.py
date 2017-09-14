ce07d97cd0232ffe0bcc6e5c52a83844396a4d8e
Toke Jepsen <tokejepsen@gmail.com>
2017-09-14 12:44:32 +0200

    Validate NukeStudio trackItem names to have four digits on the sequence name.

--- environment/PYBLISHPLUGINPATH/nukestudio/validate_track_item_name.py
+++ environment/PYBLISHPLUGINPATH/nukestudio/validate_track_item_name.py
@@ -19,11 +19,11 @@ class ValidateTrackItemName(pyblish.api.InstancePlugin):
         if instance.data["family"] != "trackItem":
             return
 
-        pattern = r"sq[0-9]{3}--sh[0-9]{4}"
+        pattern = r"sq[0-9]{4}--sh[0-9]{4}"
 
         failure_message = (
             'The track item "{0}" is not named according to the studios naming'
-            ' convention. The naming convention follows "sq%03d--sh%04d", for '
-            'example "sq001--sh0010"'.format(instance.data["item"].name())
+            ' convention. The naming convention follows "sq%04d--sh%04d", for '
+            'example "sq0010--sh0010"'.format(instance.data["item"].name())
         )
         assert re.match(pattern, instance.data["item"].name()), failure_message
