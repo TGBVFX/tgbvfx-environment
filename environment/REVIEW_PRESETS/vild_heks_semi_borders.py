0c34c2557c8b1ca9dfe0bde6d8c84a38297036bb
Toke Jepsen <tokejepsen@gmail.com>
2017-10-05 11:22:28 +0200

    Support thumbnails in process review presets.

--- environment/REVIEW_PRESETS/vild_heks_semi_borders.py
+++ environment/REVIEW_PRESETS/vild_heks_semi_borders.py
@@ -4,10 +4,27 @@ import subprocess
 
 src = sys.argv[1]
 dst = sys.argv[2]
+thumbnail = sys.argv[3]
 
+# Generate review movie
 args = [
-    "ffmpeg", "-y", "-i", src,
-    "-vf", "drawbox=c=black@0.5:t=148:x=-t:w=iw+(t*2)", "-crf", "0", dst
+    "ffmpeg", "-y",
+    "-i", src,
+    "-vf", "drawbox=c=black@0.5:t=148:x=-t:w=iw+(t*2)",
+    "-crf", "0",
+    dst
+]
+
+subprocess.call(args)
+
+# Generate thumbnail
+args = [
+    "ffmpeg", "-y",
+    "-i", src,
+    "-vf", "drawbox=c=black@0.5:t=148:x=-t:w=iw+(t*2),select=gte(n\,1),"
+    "scale=300:-1",
+    "-vframes", "1",
+    thumbnail
 ]
 
 subprocess.call(args)
