6e4ad06e1ae420e5021eea6b7ae70e0c312c855d
Toke Jepsen <tokejepsen@gmail.com>
2017-10-05 14:49:04 +0200

    No more copying of event script to scene level.

--- environment/PYBLISHPLUGINPATH/royalrender/extract_event_script.py
+++ environment/PYBLISHPLUGINPATH/royalrender/extract_event_script.py
@@ -1,66 +1,62 @@
 import os
-import shutil
 
 import pyblish.api as api
-from dirsync import sync
 
 
 class TGBVFXEnvironmentRoyalRenderExtractEventScript(api.InstancePlugin):
-    """ Add event script for updating Ftrack status. """
+    """ Add event script for updating Ftrack status.
 
-    order = api.ExtractorOrder
+    Offset to fetch Nuke job.
+    """
+
+    order = api.ExtractorOrder + 0.1
     label = "Event Script"
-    families = ["royalrender", "ftrack"]
-    match = api.Subset
+    families = ["royalrender"]
+    targets = ["process.royalrender"]
 
     def process(self, instance):
 
-        data = instance.data.get("royalrenderData", {})
-
-        directory = os.path.join(
-            os.path.dirname(instance.context.data["currentFile"]),
-            "workspace",
-            "royalrender"
+        event_script_path = os.path.join(
+            os.path.dirname(__file__),
+            "event_script",
+            "event_script.py"
+        )
+        pythonpath = os.path.join(
+            os.path.dirname(__file__), "PYTHONPATH"
         )
 
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        # Check if event script is local
+        if event_script_path.lower().startswith("c"):
+            network_path = (
+                "//10.11.0.184/171000_TGB_Library/pipeline/repositories/"
+                "tgbvfx-environment/tgbvfx-environment/environment/"
+                "PYBLISHPLUGINPATH/royalrender/event_script/event_script.py"
+            )
+            self.log.warning(
+                "\"{0}\" is a local path. Hardcoding to network path: "
+                "\"{1}\"".format(event_script_path, network_path)
+            )
+            event_script_path = network_path
 
-        destination = os.path.join(directory, "event_script.py")
-        shutil.copy(
-            os.path.join(
-                os.path.dirname(__file__),
-                "event_script",
-                "event_script.py"
-            ),
-            destination
-        )
+        # Check if pythonpath is local
+        if pythonpath.lower().startswith("c"):
+            network_path = (
+                "//10.11.0.184/171000_TGB_Library/pipeline/repositories/"
+                "tgbvfx-environment/tgbvfx-environment/environment/"
+                "PYBLISHPLUGINPATH/royalrender/PYTHONPATH"
+            )
+            self.log.warning(
+                "\"{0}\" is a local path. Hardcoding to network path: "
+                "\"{1}\"".format(pythonpath, network_path)
+            )
+            pythonpath = network_path
 
-        # Add required custom variables
-        data["CustomEventScript"] = destination
-        data["CustomFTRACK_SERVER"] = os.environ["FTRACK_SERVER"]
-        data["CustomFTRACK_APIKEY"] = os.environ["FTRACK_APIKEY"]
-        data["CustomLOGNAME"] = os.environ["LOGNAME"]
-        data["CustomFTRACK_TASKID"] = os.environ["FTRACK_TASKID"]
+        for job in instance.data["royalrenderJobs"]:
 
-        # Copy required files
-        directory = os.path.join(
-            os.path.dirname(instance.context.data["currentFile"]),
-            "workspace",
-            "royalrender",
-            "PYTHONPATH"
-        )
-
-        sync(
-            os.path.join(os.path.dirname(__file__), "PYTHONPATH"),
-            directory,
-            "sync",
-            create=True,
-            purge=True,
-            modtime=True
-        )
-
-        data["CustomPYTHONPATH"] = directory
-
-        # Setting data
-        instance.data["royalrenderData"] = data
+            # Add required custom variables
+            job["CustomEventScript"] = event_script_path
+            job["CustomFTRACK_SERVER"] = os.environ["FTRACK_SERVER"]
+            job["CustomFTRACK_APIKEY"] = os.environ["FTRACK_APIKEY"]
+            job["CustomLOGNAME"] = os.environ["LOGNAME"]
+            job["CustomFTRACK_TASKID"] = os.environ["FTRACK_TASKID"]
+            job["CustomPYTHONPATH"] = pythonpath
