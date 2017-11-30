6dc7599591e50b2022fa2e7ac3cee7996bdd7bd7
Toke Jepsen <tokejepsen@gmail.com>
2017-11-30 16:20:49 +0100

    Add Project name to RoyalRender settings.

--- environment/PYBLISHPLUGINPATH/royalrender/extract_settings.py
+++ environment/PYBLISHPLUGINPATH/royalrender/extract_settings.py
@@ -14,6 +14,7 @@ class TGBVFXEnvironmentRoyalRenderExtractSettingsNuke(api.InstancePlugin):
     targets = ["process.royalrender"]
 
     def process(self, instance):
+        import os
 
         for job in instance.data["royalrenderJobs"]:
             if job["Software"] != "Nuke":
@@ -24,4 +25,9 @@ class TGBVFXEnvironmentRoyalRenderExtractSettingsNuke(api.InstancePlugin):
             submit_params.append("SeqDivMINComp=1~5")
             submit_params.append("SeqDivMAXComp=1~40")
             submit_params.append("DefaultClientGroup=1~NODES_2D")
+            submit_params.append(
+                "CompanyProjectName=0~{0}".format(
+                    os.environ["PROJECT_PATH"].split(os.sep)[-1]
+                )
+            )
             job["SubmitterParameter"] = submit_params
